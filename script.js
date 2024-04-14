793e1b9f1141d1123439d037336b9b4e1d7bbd45
blazaartn <164594016+blazaartn@users.noreply.github.com>
2024-04-14 19:08:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,187 @@
+
+const btn1 =document.getElementById("btn1");
+const text1 =document.querySelector(".p1");
+btn1.addEventListener("click", function() {
+    const text =text1.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn2 =document.getElementById("btn2");
+const text2 =document.querySelector(".p2");
+btn2.addEventListener("click", function() {
+    const text =text2.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn3 =document.getElementById("btn3");
+const text3 =document.querySelector(".p3");
+btn3.addEventListener("click", function() {
+    const text =text3.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn4 =document.getElementById("btn4");
+const text4 =document.querySelector(".p4");
+btn4.addEventListener("click", function() {
+    const text =text4.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn5 =document.getElementById("btn5");
+const text5 =document.querySelector(".p5");
+btn5.addEventListener("click",function() {
+    const text =text5.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn6 =document.getElementById("btn6");
+const text6 =document.querySelector(".p6");
+btn6.addEventListener("click",function() {
+    const text = text6.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn7 =document.getElementById("btn7");
+const text7 =document.querySelector(".p7");
+btn7.addEventListener("click",function() {
+    const text =text7.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn8 =document.getElementById("btn8");
+const text8 =document.querySelector(".p8");
+btn8.addEventListener("click",function() {
+    const text =text8.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied successfully !");
+
+});
+const btn9 =document.getElementById("btn9");
+const text9 =document.querySelector(".p9");
+btn9.addEventListener("click",function() {
+    const text =text9.textContent;
+    navigator.clipboard.writeText(text);
+        alert("code copied successfully !");
+   
+});
+const btn10 =document.getElementById("btn10");
+const text10 =document.querySelector(".p10");
+btn10.addEventListener("click",function() {
+    const text =text10.textContent;
+    navigator.clipboard.writeText(text);
+        alert("code copied successfully !");
+});
+const btn11 =document.getElementById("btn11");
+const text11 =document.querySelector(".p11");
+btn11.addEventListener("click",function() {
+    const text =text11.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn12 =document.getElementById("btn12");
+const text12 =document.querySelector(".p12");
+btn12.addEventListener("click",function() {
+    const text =text12.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied successfully !");
+});
+const btn13 =document.getElementById("btn13");
+const text13 =document.querySelector(".p13");
+btn13.addEventListener("click",function() {
+    const text =text13.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn14 =document.getElementById("btn14");
+const text14 =document.querySelector(".p14");
+btn14.addEventListener("click",function() {
+    const text =text14.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied successfully !");
+});
+const btn15 =document.getElementById("btn15");
+const text =document.querySelector(".p15");
+btn15.addEventListener("click", function() {
+    const text15 =text15.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn16 =document.getElementById("btn16");
+const text16 =document.querySelector(".p16");
+btn16.addEventListener("click", function() {
+    const text =text16.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn17 =document.getElementById("btn17");
+const text17 =document.querySelector(".p17");
+btn17.addEventListener("click", function() {
+    const text =text17.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+});
+const btn18 =document.getElementById("btn18");
+const text18 =document.querySelector(".p18");
+btn18.addEventListener("click", function() {
+    const text =text18.textContent;
+    navigator.clipboard.writeText(text);
+    alert("code copied sccessfully");
+})
+const btn19=document.getElementById("btn19")
+const text19 =document.querySelector(".p19");
+btn19.addEventListener("click", function() {
+     const text =text19.textContent;
+     navigator.clipboard.writeText(text);
+     alert("code copied sccessfully");
+ });
+const btn20 =document.getElementById("btn20");
+const text20 =document.querySelector(".p20");
+btn20.addEventListener("click", function() {
+     const text =text20.textContent;
+     navigator.clipboard.writeText(text);
+     alert("code copied sccessfully");
+ });
+const btn21 =document.getElementById("btn21");
+const text21 =document.querySelector(".p21");
+btn4.addEventListener("click", function() {
+     const text =text21.textContent;
+     navigator.clipboard.writeText(text);
+     alert("code copied sccessfully");
+ });
+const btn22 =document.getElementById("btn22");
+const text22 =document.querySelector(".p22");
+btn22.addEventListener("click", function() {
+     const text =text22.textContent;
+     navigator.clipboard.writeText(text);
+     alert("code copied sccessfully");
+ });
+const btn23 =document.getElementById("btn23");
+const text23 =document.querySelector(".p23");
+btn23.addEventListener("click", function() {
+     const text = text23.textContent;
+     navigator.clipboard.writeText(text);
+     alert("code copied sccessfully");
+ });
+const btn24 =document.getElementById("btn24");
+const text24 =document.querySelector(".p24");
+btn24.addEventListener("click", function() {
+     const text =text24.textContent;
+     navigator.clipboard.writeText(text);
+     alert("code copied sccessfully");
+ });
+const btn25 =document.getElementById("btn25");
+const text25 =document.querySelector(".p25");
+btn8.addEventListener("click", function() {
+     const text =text25.textContent;
+     navigator.clipboard.writeText(text);
+     alert("code copied successfully !");
+
+ });
+const btn26 =document.getElementById("btn26");
+const text26 =document.querySelector(".p26");
+btn26.addEventListener("click", function() {
+     const text =text26.textContent;
+     navigator.clipboard.writeText(text);
+         alert("code copied successfully !");
+     });
+
